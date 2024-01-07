1dad8bef7e4020fcabf0dabd70e130c03dd1ab4a
Md-Nafees-Zaman <zamannafees635@gmail.com>
2024-01-07 11:28:19 +0600

    initial commit

---
+++ basic_led_check/basic_led_check.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(15, OUTPUT);
+pinMode(16, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(15, HIGH);
+digitalWrite(16,LOW);
+}
